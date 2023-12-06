d725dbefd12673772b16c2e7aeda73e39de6c640
Luis E. Galindo Amaya <78140218+Galindo-lab@users.noreply.github.com>
2023-12-05 21:02:04 -0800

    Ejemplo correos agregado

--- PEMA/views.py
+++ PEMA/views.py
@@ -5,4 +5,14 @@ from django.http import HttpResponse
 
 # Create your views here.
 def test(request):
+    """send_mail(
+        subject="Email de prueba",
+        message="Hola, estoy enviando correos electrónicos desde Django. Si estás recibiendo esto, es porque la "
+                "prueba fue exitosa. Atentamente, Galindo.",
+        from_email=settings.EMAIL_HOST_USER,
+        fail_silently=False,
+        recipient_list=[
+            "egalindo54@uabc.edu.mx"
+        ]
+    )"""
     return HttpResponse("OK")
