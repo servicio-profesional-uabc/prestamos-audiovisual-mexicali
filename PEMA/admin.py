e28f21e4928069ec1429bd4e453cc8f5c8438671
galindo <egalindo54@uabc.edu.mx>
2024-02-18 00:03:04 -0800

    modelos registrados en el panel de admin

--- PEMA/admin.py
+++ PEMA/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Prestatario)
+admin.site.register(Carrito)
+admin.site.register(Profesor)
+admin.site.register(Alumno)
+admin.site.register(Materia)
+admin.site.register(AlumnoMateria)
+admin.site.register(Coordinador)
+admin.site.register(Almacen)
+admin.site.register(Orden)
+admin.site.register(Reporte)
+admin.site.register(Articulo)
+admin.site.register(Unidad)
+admin.site.register(Categoria)
+admin.site.register(AutorizacionAlmacen)
+admin.site.register(AutorizacionCoordinador)
+admin.site.register(ProfesorMateria)
+admin.site.register(ArticuloCarrito)
+admin.site.register(ArticuloMateria)
+admin.site.register(CategoriaArticulo)
