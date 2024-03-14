2fef947701ade9eab4e39a4d6dfe4906e2176fef
FranciscoJHOrnelas <fhernandez80@uabc.edu.mx>
2024-03-13 17:06:17 -0700

    Login y logout

--- PEMA/urls.py
+++ PEMA/urls.py
@@ -1,15 +1,23 @@
-from django.urls import path
+from django.urls import include, path
 from . import views
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
+    #path(
+    #    route='',
+    #    view=views.IndexView.as_view(),
+    #    name='index'
+    #),
+    #path(
+    #    route='test/',
+    #    name='test',
+    #    view=views.test
+    #),
     path(
-        route='',
-        view=views.IndexView.as_view(),
-        name='index'
+        'accounts/', include('django.contrib.auth.urls')
     ),
     path(
-        route='test/',
-        name='test',
-        view=views.test
+        '', TemplateView.as_view(template_name='index.html'), name='index'
     ),
+    
 ]
