166726d5e6cfe4d458b859917ccc41f544cc04c9
DiegoAndresGlez <diegoglez077@gmail.com>
2024-04-01 12:02:08 -0700

    dockerized - no terminado aun

--- prestamos/settings.py
+++ prestamos/settings.py
@@ -11,9 +11,13 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir' .
-BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Before docker
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -24,7 +28,9 @@ SECRET_KEY = 'django-insecure-6b00ecn+-56drvcpf_u(1ts&=s$l1v@5k72e9c3sowawe%27xj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# Before docker
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 # Application definition
 
@@ -72,10 +78,20 @@ WSGI_APPLICATION = 'prestamos.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pema-db',
+        'USER': 'root',
+        'PASSWORD': 'P@ssword123',
+        'HOST': 'db',
+        'PORT': '3306',
     }
 }
 
@@ -111,7 +127,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# Static files
+STATIC_URL = '/static/'
+STATIC_ROOT = '/app/staticfiles/'
+
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/app/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
