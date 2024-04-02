979c49cd5fd7815f7c06ee9cc9ed8ef747d8936e
Milks002 <a1283472@uabc.edu.mx>
2024-04-02 15:13:20 -0700

    Login Funcionando

--- prestamos/settings.py
+++ prestamos/settings.py
@@ -112,6 +112,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+LOGIN_REDIRECT_URL = 'menu'
+LOGOUT_REDIRECT_URL = 'home'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
