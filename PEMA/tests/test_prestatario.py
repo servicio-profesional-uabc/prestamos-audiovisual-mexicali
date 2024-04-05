3168159235f633f433535acf75200231a698b16e
galindo <egalindo54@uabc.edu.mx>
2024-04-05 11:53:18 -0700

    materia agregado y estado creado

--- PEMA/tests/test_prestatario.py
+++ PEMA/tests/test_prestatario.py
@@ -15,15 +15,19 @@ class TestPrestatario(TestCase):
         self.user_prestatario = Prestatario.crear_usuario(id=1, username="prestatario", password="<PASSWORD>")
         self.user_almacen = Almacen.crear_usuario(id=2, username="almacen", password="<PASSWORD>")
 
+        materia = Materia.objects.create(nombre="Fotografia2", periodo="2024-1")
+
         # ordenes
         self.orden1 = Orden.objects.create(
+            materia=materia,
             prestatario=self.user_prestatario,
-            lugar=Orden.Ubicacion.CAPUS,
+            lugar=Orden.Ubicacion.CAMPUS,
             inicio=make_aware(datetime(2024, 10, 5)),
             final=make_aware(datetime(2024, 10, 5))
         )
 
         self.orden2 = Orden.objects.create(
+            materia=materia,
             prestatario=self.user_prestatario,
             lugar=Orden.Ubicacion.EXTERNO,
             inicio=make_aware(datetime(2024, 10, 5)),
