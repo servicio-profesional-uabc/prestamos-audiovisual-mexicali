3c27147768d693266c83b3359b634b7852762a71
galindo <egalindo54@uabc.edu.mx>
2024-04-20 13:43:35 -0700

    Pantalla de Orden en el admin actualizada

--- PEMA/admin.py
+++ PEMA/admin.py
@@ -1,16 +1,6 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Prestatario)
-admin.site.register(Almacen)
-admin.site.register(Maestro)
-admin.site.register(Coordinador)
-admin.site.register(Perfil)
-
-admin.site.register(Entrega)
-admin.site.register(Devolucion)
-admin.site.register(Carrito)
-
 
 class MateriaAdmin(admin.ModelAdmin):
     """
@@ -33,9 +23,6 @@ class OrdenAdmin(admin.ModelAdmin):
 admin.site.register(Orden, OrdenAdmin)
 
 
-admin.site.register(Reporte)
-
-
 class ArticuloAdmin(admin.ModelAdmin):
     """
     Admin panel management for Alumni
@@ -46,11 +33,15 @@ class ArticuloAdmin(admin.ModelAdmin):
 
 admin.site.register(Articulo, ArticuloAdmin)
 
+
+admin.site.register(Perfil)
+admin.site.register(Entrega)
+admin.site.register(Devolucion)
+admin.site.register(Carrito)
 admin.site.register(Unidad)
 admin.site.register(Categoria)
-
 admin.site.register(AutorizacionOrden)
-
 admin.site.register(ArticuloCarrito)
 admin.site.register(ArticuloMateria)
 admin.site.register(CorresponsableOrden)
+admin.site.register(Reporte)
