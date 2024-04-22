d7924e4716fb6c1cb54f8ea7238ed407ab25861c
galindo <egalindo54@uabc.edu.mx>
2024-04-22 13:57:00 -0700

    login redirect funcionando

--- prestamos/settings.py
+++ prestamos/settings.py
@@ -115,7 +115,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 LOGIN_REDIRECT_URL = 'menu'
-LOGOUT_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = ''
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
